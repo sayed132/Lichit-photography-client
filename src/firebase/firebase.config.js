403fb841dd4600098d1cab81ec9d32a32abd3c6d
Abu Sayed <rafsansayed132@gmail.com>
2022-11-10 21:00:53 +0600

    added env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHfQ3X7sPzBTX1MzOV_6liHcdwSLWJrvc",
-  authDomain: "rst-digital.firebaseapp.com",
-  projectId: "rst-digital",
-  storageBucket: "rst-digital.appspot.com",
-  messagingSenderId: "45443894689",
-  appId: "1:45443894689:web:5477b20037fcc082270fe0"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
